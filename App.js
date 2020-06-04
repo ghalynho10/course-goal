4e5a2925b446ecdbd8fb219acb31858b9176ca3f
ghalynho10 <mghalynho@gmail.com>
2020-06-04 12:08:41 -0400

    Splitting app into components

--- App.js
+++ App.js
@@ -1,18 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import GoalItem from "./components/GoalInput";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [enteredGoal, setEntertedGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => setEntertedGoal(enteredText);
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: enteredGoal },
+    ]);
+  };
+
   return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: "center" }}>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="Course Goal"
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }} />
-        <Button title="ADD" />
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemData) => <GoalInput title={itemData.item.value} />}
+      />
+
+      {/* ScrollView example  */}
+      {/* <ScrollView>
+        {courseGoals.map((goal, index) => (
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+});
